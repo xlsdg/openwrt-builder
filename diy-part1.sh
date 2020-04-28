d0ff20c0ebbbb93c514ab3c7f189a5cb994ba219
xLsDg <xlsdg@qq.com>
2020-04-29 07:26:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,4 +13,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '$a src-git clash https://github.com/vernesong/OpenClash' feeds.conf.default

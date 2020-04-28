019cbbc30c5857c8c75d678e632db50d6f9f4da9
xLsDg <xlsdg@qq.com>
2020-04-29 07:44:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,5 +12,14 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git clash https://github.com/vernesong/OpenClash' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+cd -

50d00d97f4f82bf409e97c62cede978903128146
Zhentian Lu <zhentian.lu@compass.com>
2023-07-18 15:59:57 +0800

    Update

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
 echo 'src-git clash https://github.com/vernesong/OpenClash' >>feeds.conf.default

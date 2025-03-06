76c41ef10396e5a1c85b7c299942f86cd5fafed9
Zhentian Lu <xlsdg@qq.com>
2025-03-06 10:53:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git clash https://github.com/vernesong/OpenClash.git;dev' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >>feeds.conf.default

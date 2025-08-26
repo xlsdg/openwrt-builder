01da6366b1cad17461373b9ff45074d2d5b56f38
Zhentian Lu <xlsdg@qq.com>
2025-08-26 16:44:19 +0800

    Comment out OpenClash feed source addition

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >>feeds.conf.default
+#echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >>feeds.conf.default
